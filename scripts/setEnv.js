d2f9489a80e05989c666bf18d558622e90a47479
PabloCanarteSol <120370719+PabloCanarteSol@users.noreply.github.com>
2025-02-11 16:57:03 +0100

    Update setEnv.js

--- scripts/setEnv.js
+++ scripts/setEnv.js
@@ -1,56 +1,36 @@
+
 import dotenv from 'dotenv';
 dotenv.config();
 import fs from "fs";
 
-let secretValue = "";
 
-if (process.env.OS === "ios") {
+function write(updatedEnv){
+  console.log("what"+updatedEnv)
+
+  let secretValue = "";
+  if (process.env.OS === "ios") {
     secretValue = process.env.PERCY_TOKEN_IOS;
-} else if (process.env.OS === "android") {
-    secretValue = process.env.PERCY_TOKEN_ANDROID;
-} else {
-      process.exit(1);
-}
-import dotenv from 'dotenv';
-dotenv.config();
-import fs from "fs";
+  } else if (process.env.OS === "android") {
+      secretValue = process.env.PERCY_TOKEN_ANDROID;
+  } else {
+        process.exit(1);
+  }
+  if (updatedEnv.includes('PERCY_TOKEN=')) {
+      updatedEnv = updatedEnv.replace(/PERCY_TOKEN=.*/g, `PERCY_TOKEN=${secretValue}`);
+  } else {
+      updatedEnv += `PERCY_TOKEN=${secretValue}\n`;
+  }
+  fs.writeFileSync('.env', updatedEnv);
+}  
 
-let secretValue = "";
 
-if (process.env.OS === "ios") {
-    secretValue = process.env.PERCY_TOKEN_IOS;
-} else if (process.env.OS === "android") {
-    secretValue = process.env.PERCY_TOKEN_ANDROID;
-} else {
-      process.exit(1);
-}
+
 
 try{
   const dotenvFile = fs.readFileSync('.env', 'utf8');
-  let updatedEnv = dotenvFile;
+  write(dotenvFile);
 
 } catch (error) {
-  let updatedEnv = '';
+  write(''); 
 
 }
-
-
-if (updatedEnv.includes('PERCY_TOKEN=')) {
-    updatedEnv = updatedEnv.replace(/PERCY_TOKEN=.*/g, `PERCY_TOKEN=${secretValue}`);
-  } else {
-    updatedEnv += `PERCY_TOKEN=${secretValue}\n`;
-  }
-  
-  fs.writeFileSync('.env', updatedEnv);
-  
-
-
-
-if (updatedEnv.includes('PERCY_TOKEN=')) {
-    updatedEnv = updatedEnv.replace(/PERCY_TOKEN=.*/g, `PERCY_TOKEN=${secretValue}`);
-  } else {
-    updatedEnv += `PERCY_TOKEN=${secretValue}\n`;
-  }
-  
-  fs.writeFileSync('.env', updatedEnv);
-  
