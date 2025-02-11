a70629b07590d77c116c34e24e7ad4d1e59f83e7
Pipe <devops@esmorga.canarte.org>
2025-02-11 22:12:29 +0100

    TEST

--- scripts/setEnv.js
+++ scripts/setEnv.js
@@ -5,8 +5,7 @@ import fs from "fs";
 
 
 function write(updatedEnv){
-  console.log("what"+updatedEnv)
-
+  console.log("1")
   let secretValue = "";
   if (process.env.OS === "ios") {
     secretValue = process.env.PERCY_TOKEN_IOS;
@@ -15,12 +14,15 @@ function write(updatedEnv){
   } else {
         process.exit(1);
   }
+  console.log("2")
   if (updatedEnv.includes('PERCY_TOKEN=')) {
       updatedEnv = updatedEnv.replace(/PERCY_TOKEN=.*/g, `PERCY_TOKEN=${secretValue}`);
   } else {
       updatedEnv += `PERCY_TOKEN=${secretValue}\n`;
   }
+  console.log("3")
   fs.writeFileSync('.env', updatedEnv);
+  console.log("4")
 }  
 
 
