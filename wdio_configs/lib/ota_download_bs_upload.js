e9101772c782c0f62f40788f1b6d701bfc4e2574
Pipe <devops@esmorga.canarte.org>
2025-02-11 13:23:08 +0100

    Refactor 2

---
+++ wdio_configs/lib/ota_download_bs_upload.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import axis from 'axios';
+
+export async function uploadToBrowserStack(url) {
+  url=url.replace('https://otashare.mobgen.com/build/', 'https://otashare.mobgen.com/build/download/').replace('/esmorga-qa', '')
+  const response = await axis.post('https://api-cloud.browserstack.com/app-automate/upload', {
+    url: url
+  }, {
+    auth: {
+      username: process.env.BROWSERSTACK_USERNAME,
+      password: process.env.BROWSERSTACK_ACCESS_KEY
+
+    }
+  });
+  return response.data.app_url;
+}
