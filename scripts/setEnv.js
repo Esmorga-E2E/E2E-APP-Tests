f0467d13da1a7ed6cb7e67d04bbd02cedf2c6184
PabloCanarteSol <120370719+PabloCanarteSol@users.noreply.github.com>
2025-02-11 16:36:08 +0100

    Update setEnv.js

--- scripts/setEnv.js
+++ scripts/setEnv.js
@@ -11,8 +11,39 @@ if (process.env.OS === "ios") {
 } else {
       process.exit(1);
 }
-const dotenvFile = fs.readFileSync('.env', 'utf8');
-let updatedEnv = dotenvFile;
+import dotenv from 'dotenv';
+dotenv.config();
+import fs from "fs";
+
+let secretValue = "";
+
+if (process.env.OS === "ios") {
+    secretValue = process.env.PERCY_TOKEN_IOS;
+} else if (process.env.OS === "android") {
+    secretValue = process.env.PERCY_TOKEN_ANDROID;
+} else {
+      process.exit(1);
+}
+
+try{
+  const dotenvFile = fs.readFileSync('.env', 'utf8');
+  let updatedEnv = dotenvFile;
+
+} catch (error) {
+  let updatedEnv = '';
+
+}
+
+
+if (updatedEnv.includes('PERCY_TOKEN=')) {
+    updatedEnv = updatedEnv.replace(/PERCY_TOKEN=.*/g, `PERCY_TOKEN=${secretValue}`);
+  } else {
+    updatedEnv += `PERCY_TOKEN=${secretValue}\n`;
+  }
+  
+  fs.writeFileSync('.env', updatedEnv);
+  
+
 
 
 if (updatedEnv.includes('PERCY_TOKEN=')) {
