33dfe24c74e0ddfa186bb9489b3c54c86c7b015d
Pipe <devops@esmorga.canarte.org>
2025-02-26 16:58:18 +0100

    Create Basic Test

---
+++ features/US/MOB-14.feature
@@ -0,0 +1,18 @@
+@MOB-14
+Feature: MOB-14
+ 
+    Scenario: TC - Login MVP - Register
+		Given just opened app
+		And user status is unregistred
+		Then wellcome screen is shown
+		When tap on primary button
+		Then login screen is shown
+		When tap on secondary button
+		Then register screen is shown
+		When write Esmorga on field name
+		And write Esmorga on field last name
+		And write prueba@esmorga.canarte.org on field email
+		And write Password!1 on field password
+		And write Password!1 on field retry password
+		And tap on primary button
+		
---
+++ screenobjects/register.screen.js
@@ -0,0 +1,83 @@
+import Basics from './basic.screen'
+export default class Register extends Basics {
+
+    get_what_to_seek(reg) {
+        switch (browser.capabilities.platformName) {
+            case "Android":
+            case "android":
+                return '//android.widget.TextView[@text="Register"]'
+            case "iOS":
+            case "ios":
+                return '//XCUIElementTypeStaticText[@name="RegistrationView.title"]'
+        }
+
+    }
+    get_where_tap_on(where){
+        switch(where){
+            case 'primary button':
+                switch (browser.capabilities.platformName) {
+                    case "Android":
+                    case "android":
+                        return '//android.widget.ScrollView/android.view.View[2]/android.widget.Button'
+                                
+                    case "iOS":
+                    case "ios":
+                        return '//XCUIElementTypeButton[2]'
+                }
+            }
+    }  
+    get_where_to_write(where){
+        switch(where){
+            case 'name':
+                switch (browser.capabilities.platformName) {
+                    case "Android":
+                    case "android":
+                        return '//android.widget.ScrollView/android.widget.EditText[1]'
+                    case "iOS":
+                    case "ios":
+                        return '//XCUIElementTypeStaticText[@name="RegistrationView.textfield0"]'
+                }
+
+                case 'last name':
+                    switch (browser.capabilities.platformName) {
+                        case "Android":
+                        case "android":
+                            return '//android.widget.ScrollView/android.widget.EditText[2]'
+                        case "iOS":
+                        case "ios":
+                            return '//XCUIElementTypeStaticText[@name="RegistrationView.textfield1"]'
+                    }
+
+            case 'email':
+                switch (browser.capabilities.platformName) {
+                    case "Android":
+                    case "android":
+                        return '//android.widget.ScrollView/android.widget.EditText[3]'
+                    case "iOS":
+                    case "ios":
+                        return '//XCUIElementTypeStaticText[@name="RegistrationView.textfield2"]'
+                }
+                case 'password':
+                    switch (browser.capabilities.platformName) {
+                        case "Android":
+                        case "android":
+                            return '//android.widget.ScrollView/android.widget.EditText[4]'
+                        case "iOS":
+                        case "ios":
+                            return '//XCUIElementTypeStaticText[@name="RegistrationView.textfield3"]'
+                        }
+                
+                case 'retry password':
+                    switch (browser.capabilities.platformName) {
+                        case "Android":
+                        case "android":
+                            return '//android.widget.ScrollView/android.widget.EditText[5]'
+                        case "iOS":
+                        case "ios":
+                            return '//XCUIElementTypeStaticText[@name="RegistrationView.textfield4"]'
+                        }
+                }
+    
+            }
+
+}
