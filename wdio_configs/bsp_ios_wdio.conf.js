1875f224889416a8297a07d2cdc00c7c2a7e5d54
Pipe <devops@esmorga.canarte.org>
2025-03-18 14:56:08 +0100

    FIX pipeline tests

--- wdio_configs/bsp_ios_wdio.conf.js
+++ wdio_configs/bsp_ios_wdio.conf.js
@@ -2,14 +2,6 @@ const appPackage = 'com.mobilestudio.esmorga';
 import { config as basic_config }  from "./bs_ios_wdio.conf.js"
 export const config = { ...basic_config, 
 
-capabilities: [{
-  'bstack:options': {
-    deviceName: 'iPhone 16',
-    platformVersion: '18',
-    platformName: 'ios',
-  },
-
-}],
 BeforeAll: async function (world, context) {
   driver.execute_script("mobile: shell", {
     "command": "defaults",
